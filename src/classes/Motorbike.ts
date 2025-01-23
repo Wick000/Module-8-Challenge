3e8ff21076b7bc283f06171f95fa220e3cc01720
Wick000 <chad.abramson987@gmail.com>
2025-01-22 22:29:05 -0500

    Night push

--- src/classes/Motorbike.ts
+++ src/classes/Motorbike.ts
@@ -2,10 +2,72 @@
 import Vehicle from './Vehicle.js';
 import Wheel from './Wheel.js';
 
+interface Wheelie {
+  wheelie: () => string;
+}
+
 // TODO: The Motorbike class should extend the Vehicle class
-class Motorbike {
+class Motorbike extends Vehicle implements Wheelie{
   // TODO: Declare properties of the Motorbike class
   // TODO: The properties should include vin, color, make, model, year, weight, top speed, and wheels
+  vin: string;
+  color: string;
+  make: string;
+  model: string;
+  year: number;
+  weight: number;
+  topSpeed: number;
+  wheels: Wheel[];
+
+  constructor(
+    vin: string,
+    color: string,
+    make: string,
+    model: string,
+    year: number,
+    weight: number,
+    topSpeed: number,
+    wheels: Wheel[],
+  ) {
+    super();
+
+    this.vin = vin;
+    this.color = color;
+    this.make = make;
+    this.model = model;
+    this.year = year;
+    this.weight = weight;
+    this.topSpeed = topSpeed;
+    
+    if (wheels.length !== 2) {
+      this.wheels = [new Wheel(), new Wheel(),];
+    } else {
+      this.wheels = wheels;
+    }
+     
+    }
+   wheelie(): string {
+      return `Motorbike ${this.make} ${this.model} is doing a wheelie!"`;
+    }
+override printDetails( 
+     ): void {
+       console.log(`VIN ${this.vin}`)
+       console.log(`Color: ${this.color}`);
+       console.log(`Make: ${this.make}`);
+       console.log(`Model: ${this.model}`);
+       console.log(`Year: ${this.year}`);
+       console.log(`Weight: ${this.weight}`);
+       console.log(`Top Speed: ${this.topSpeed}`)
+       console.log(`Wheeles ${this.wheels.length}`)
+    }
+  };
+
+  
+     
+      
+
+  
+
   // TODO: The types should be as follows: vin (string), color (string), make (string), model (string), year (number), weight (number), topSpeed (number), wheels (Wheel[])
 
   // TODO: Create a constructor that accepts the properties of the Motorbike class
@@ -20,7 +82,7 @@ class Motorbike {
   // TODO: The method should call the printDetails method of the parent class
   // TODO: The method should log the details of the Motorbike
   // TODO: The details should include the VIN, make, model, year, weight, top speed, color, and wheels
-}
+
 
 // Export the Motorbike class as the default export
 export default Motorbike;
